2488c9a093c2ed8de73f31ff10699252ffaf19a3
v-liberov <v-liberov@yandex-team.ru>
2019-03-01 15:06:23 +0700

    initialize Xcode project

---
+++ parrotWings/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  parrotWings
+//
+//  Created by Vsevolod Liberov on 01/03/2019.
+//  Copyright © 2019 Seva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
